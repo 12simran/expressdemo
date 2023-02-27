0b10b8411bda048ee723f7d8bec05ad07fbfc7b2
12simran <simrankaur283380@gmail.com>
2023-02-27 18:13:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+
+const express = require('express')
+const port =3000
+const app = express()
+let ejs = require('ejs')
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use(express.urlencoded())
+
+
+var contactList = [
+    {
+        name: "Rio",
+        phone: "1111111111"
+    },
+    {
+        name: "Tony Stark",
+        phone: "1234567890"
+    },
+    {
+        name: "Richard",
+        phone: "12131321321"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.render('index',{
+        title:"index",
+        list:contactList
+       
+               
+      
+    })
+  })
+  app.get('/project', (req, res) => {
+    res.render('project',{
+        title:"project",
+        list:contactList
+       
+    })
+  })
+
+  app.post('/create',(req,res) => {
+    console.log(req.body);
+    contactList.push(req.body)
+    return res.redirect("back")
+  })
+ /* app.get('/project', (req, res) => {
+    res.render('project',{
+        title:"project_page",
+        user:{
+            name:"simran",
+            course:"full stack"
+        }
+    })
+  })*/
+
+  app.listen(port)
+  console.log("serving is running@",port)
